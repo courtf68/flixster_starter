705b24fa037c0c87e269adce73ce8529fc1be0f9
Courtney Fenderson <cfenderson@cfender-ltmv2wd.internal.salesforce.com>
2022-06-19 21:30:50 -0700

    almost

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const api_key = "0522034165f29fe2c0404b02abc42f40";
 const star = "&#10032;";
 
+const num = 1;
 const searchT = document.getElementById("query"); //searxch box
 const theForm = document.getElementById("looky"); //my form
 const press = document.getElementById("submit"); //button
 const dataSec = document.querySelector(".page"); //where data goes
 const title = document.getElementById("Ptitle"); //changing title of screen
 const searchSection = document.querySelector(".searchSection");
+const home = document.getElementById("home");
+const load = document.getElementById("load-more-movies-btn");
 
 const url = `https://api.themoviedb.org/3/movie/550?api_key=${api_key}`;
 //const movieSearchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${searchT}&page=1&include_adult=false`;
@@ -43,6 +46,11 @@ function displayResults(data) {
   dataSec.innerHTML = dataSec.innerHTML + movStr;
 }
 
+load.addEventListener("click", async (event) => {
+  event.preventDefault();
+  page++;
+});
+
 function displayResults1(data) {
   const movStr = data.results
     .map(
@@ -57,10 +65,12 @@ function displayResults1(data) {
     <h3 class = "movie-title">${movie.title} </h3>
   
     </div>
+   
     </div>
 `
     )
     .join("");
+
   searchSection.innerHTML = searchSection.innerHTML + movStr;
 }
 //template strs, DS is my empty div
@@ -82,14 +92,14 @@ async function NowP() {
 
 //attempting search func ++
 
-async function doSubmit(event) {
-  // dataSec.innerHTML = ""; //clearing
-  title.innerHTML = "Search Results";
-}
+// async function doSubmit(event) {
+//   // dataSec.innerHTML = ""; //clearing
+//   title.innerHTML = "Search Results";
+// }
 
 async function Searchy(tempS) {
   const response = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${tempS}&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${tempS}&page=${num}&include_adult=false`
   );
   const datay = await response.json(); //VS said no await needed
   console.log(datay);
@@ -97,7 +107,12 @@ async function Searchy(tempS) {
 }
 
 //back button, set search to hidden and run now playing again
-
+home.addEventListener("click", async (event) => {
+  searchSection.style.display = "none";
+  title.innerHTML = "Now Playing";
+  NowP();
+  dataSec.style.display = "initial";
+});
 // function displayResults(data) {
 //   const movStr = data.results.map(
 //     (movie) => ` <div class="movie-votes"> ${star}${movie.vote_average}</div>`
@@ -108,6 +123,7 @@ async function Searchy(tempS) {
 
 theForm.addEventListener("submit", async (event) => {
   event.preventDefault();
+  title.innerHTML = "Search Results";
   dataSec.style.display = "none";
   let tempS = event.target.q.value;
   console.log(tempS);
